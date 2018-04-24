8fc19909e1cb170916482a38b858537e43f9efd8
Ondra Pelech <ondra.pelech@gmail.com>
2018-04-24 23:47:31 +0200

    Successful reading of measurement data

--- illumeasure.py
+++ illumeasure.py
@@ -15,13 +15,43 @@ def bcc(i):
   res = reduce(lambda x, y: x ^ y, bytes)
   return "%02x" % res
 
-def encodeMessage(receptorHead, command, parameter):
+def encodeShort(receptorHead, command, parameter):
   bccable = receptorHead + command + parameter + "\x03" # 0x03 for ETX
   bccResult = bcc(bccable)
   return "\x02" + bccable + bccResult + "\x0D\x0A" # \x02 for STX, \x0D for CR, \x0A for LF
 
-PcConnectionMode = encodeMessage("00", "54", "1   ")
-PcConnectionModeResponse = encodeMessage("00", "54", "    ")
+def dataToNumber(i):
+  if i == "      ":
+    return None
+  else:
+    v = int(i[1:5])
+    e = int(i[5]) - 4
+    r = v * (10**e)
+    return -r if i[0] == '-' else r
+
+def decodeLong(i):
+  expectedBcc = bcc(i[1:28])
+  actualBcc = i[28:30]
+  if int(actualBcc, 16) != int(expectedBcc, 16):
+    raise ValueError("BCC check failed, expected BCC '%s', got '%s', received '%s'." % (expectedBcc, actualBcc, i))
+  
+  receptorHead = i[1:3]
+  command = i[3:5]
+  status = i[5:9]
+  data1 = dataToNumber(i[9:15])
+  data2 = dataToNumber(i[15:21])
+  data3 = dataToNumber(i[21:27])
+  
+  return (receptorHead, command, status, (data1, data2, data3))
+
+shortLength = 14
+longLength = 32
+
+pcConnectionMode = encodeShort("00", "54", "1   ")
+pcConnectionModeResponse = encodeShort("00", "54", "    ")
+
+setMeasurementConditions01 = encodeShort("01", "10", "0200")
+#setMeasurementConditions01Response = encodeShort("01", "10", "   0")
 
 def main():
 
@@ -91,17 +121,16 @@ def main():
       os._exit(1)
 
 
-  ret = ftdi.write_data(ftdic, PcConnectionMode)
+  ret = ftdi.write_data(ftdic, pcConnectionMode)
   if ret < 0:
       print('ftdi_write_data failed: %d (%s)' %
             (ret, ftdi.get_error_string(ftdic)))
       os._exit(1)
   else:
-    print('ftdi_write_data wrote "%s", %d bytes' % (PcConnectionMode, ret))
-  time.sleep(1)
+    print('ftdi_write_data wrote "%s", %d bytes' % (pcConnectionMode, ret))
+  time.sleep(0.1)
 
-  #response = bytearray(64)
-  ret, response = ftdi.read_data(ftdic, 14)
+  ret, response = ftdi.read_data(ftdic, shortLength)
   if ret < 0:
       print('ftdi_read_data failed: %d (%s)' %
             (ret, ftdi.get_error_string(ftdic)))
@@ -109,13 +138,35 @@ def main():
   else:
       print('ftdi_read_data read "%s", %d bytes' % (response, ret))
   
-  if response == PcConnectionModeResponse:
+  if response == pcConnectionModeResponse:
     print("Successfully set to PC mode")
   else:
-    print('wrong PcConnectionMode response, expected "%s", got "%s".' % (PcConnectionModeResponse, response))
+    print('wrong PcConnectionMode response, expected "%s", got "%s".' % (pcConnectionModeResponse, response))
+    os._exit(1)
+  time.sleep(0.5)
 
 
+  ret = ftdi.write_data(ftdic, setMeasurementConditions01)
+  if ret < 0:
+      print('ftdi_write_data failed: %d (%s)' %
+            (ret, ftdi.get_error_string(ftdic)))
+      os._exit(1)
+  else:
+    print('ftdi_write_data wrote "%s", %d bytes' % (setMeasurementConditions01, ret))
+  time.sleep(0.1)
 
+
+  ret, response = ftdi.read_data(ftdic, longLength)
+  if ret < 0:
+      print('ftdi_read_data failed: %d (%s)' %
+            (ret, ftdi.get_error_string(ftdic)))
+      os._exit(1)
+  else:
+      print('ftdi_read_data read "%s", %d bytes' % (response, ret))
+
+  decoded = decodeLong(response)
+
+  print(decoded)
 
 
   # close usb
